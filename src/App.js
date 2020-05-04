947b85a22e623646994b5d4afb40e45a0bfb0328
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-05-04 19:49:29 +0100

    Task 2 - Add 6 show titles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import darkCover from './dark.jpg';
+import moonlight from './moonlight.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="cover">
-        <img src={darkCover}  alt="dark cover"/>
+
+
+    <div className="container">
+
+        <div className="movie-cover"><img src={moonlight}  alt="moonlight cover"/></div>
+        <br></br>
+        <br></br>
+        <br></br>
+        
+    
+        <div className="flex-items">
+          <div className="movies"><h2>Moonlight</h2></div>
+          <div className="movies"><h2>The Southern Rail experience</h2></div>
+          <div className="movies"><h2>Much Ado About Nothing</h2></div>
+        </div>
+
+        <div className="flex-items">
+          <div className="movies"><h2>Joker</h2></div>
+          <div className="movies"><h2>Inherent Vice</h2></div>
+          <div className="movies"><h2>Underwater</h2></div>
+        </div>  
+
     </div>
   );
 }
