6dcc85e65d02180af6570602697f890b0c7fc279
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-05-04 20:51:22 +0100

    Task 3 - Add covers to the titles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import moonlight from './moonlight.jpg';
+import moonlight from './images/moonlight.jpg';
+import southern from './images/rail.jpg';
+import much from './images/much.jpg';
+import joker from './images/joker.jpg';
+import inherent from './images/inherent.jpg';
+import underwater from './images/underwater.jpg';
 import './App.css';
 
 function App() {
@@ -7,24 +12,38 @@ function App() {
 
 
     <div className="container">
-
-        <div className="movie-cover"><img src={moonlight}  alt="moonlight cover"/></div>
-        <br></br>
-        <br></br>
-        <br></br>
-        
-    
-        <div className="flex-items">
-          <div className="movies"><h2>Moonlight</h2></div>
-          <div className="movies"><h2>The Southern Rail experience</h2></div>
-          <div className="movies"><h2>Much Ado About Nothing</h2></div>
+      <div className="row">
+        <div className="movies">
+          <img src={moonlight} alt="moonlight cover" />
+          <h2>Moonlight</h2>
         </div>
 
-        <div className="flex-items">
-          <div className="movies"><h2>Joker</h2></div>
-          <div className="movies"><h2>Inherent Vice</h2></div>
-          <div className="movies"><h2>Underwater</h2></div>
-        </div>  
+        <div className="movies">
+          <img src={southern} alt="southern cover" />
+          <h2>The Southern Rail experience</h2>
+        </div>
+
+        <div className="movies">
+          <img src={much} alt="much cover" />
+          <h2>Much Ado About Nothing</h2>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="movies">
+          <img src={joker} alt="joker cover" />
+          <h2>Joker</h2>
+        </div>
+
+        <div className="movies">
+          <img src={inherent} alt="inherent cover" />
+          <h2>Inherent Vice</h2>
+        </div>
+
+        <div className="movies">
+          <img src={underwater} alt="underwater cover" />
+          <h2>Underwater</h2></div>
+      </div>
 
     </div>
   );
