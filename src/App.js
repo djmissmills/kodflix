286ca3eeef40f2958dfddb818a43ac2a3fc799ca
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-05-04 17:40:33 +0100

    additional heading added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           <h1>Welcome to Kodflix</h1>
+          <h2>Home of the big screen</h2>
         </p>
         <a
           className="App-link"
