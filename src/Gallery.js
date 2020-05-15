465ae230f165279d4c85ef4d32a3a8ec8c82b05e
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-05-15 12:42:56 +0100

    Task 11 Improve the gallery layout

--- src/Gallery.js
+++ src/Gallery.js
@@ -17,8 +17,6 @@ export default function Gallery(){
           <MovieItem id='moonlight'name="moonlight" logo={moonlight}/>
           <MovieItem id='experience'name="The Experience" logo={southern}/>
           <MovieItem id='much'name="Much A Doo" logo={much}/>
-        </div>
-        <div className="row">
           <MovieItem id='joker'name="Joker" logo={joker}/>
           <MovieItem id='inherent'name="Inherent" logo={inherent}/>
           <MovieItem id='underwater'name="Underwater" logo={underwater}/>
