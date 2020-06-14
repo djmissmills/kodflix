60108b75d84a32d80af3c1b16541e98fa5cf3836
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-06-14 22:28:48 +0100

    #20 Improve setup of backend server. Nodemon

---
+++ src/backend/shows.js
@@ -0,0 +1,28 @@
+module.exports = function getShows() {
+    return [
+        {name: 'moonlight',
+        id:'moon',
+        synopsis:'Moonlight is a 2016 American coming-of-age drama film written and directed by Barry Jenkins, based on Tarell Alvin McCraney\'s unpublished semi-autobiographical play In Moonlight Black Boys Look Blue.'},
+
+        {name: 'Girl on train',
+        id: 'experience',
+        synopsis:'The Girl on the Train is a 2016 American mystery thriller drama film directed by Tate Taylor and written by Erin Cressida Wilson, based on British author Paula Hawkins\' popular 2015 debut novel of the same name. The film stars Emily Blunt, Rebecca Ferguson, Haley Bennett, Justin Theroux, Luke Evans, Allison Janney, Édgar Ramírez, and Lisa Kudrow. The film follows an alcoholic divorcée named Rachel who becomes involved in a missing person\'s investigation.'},
+    
+        {name:'Much A Doo',
+        id:'much',
+        synopsis:'Much Ado About Nothing is a 1993 British/American romantic comedy film based on William Shakespeare\'s play of the same name. It was adapted for the screen and directed by Kenneth Branagh, who also stars in the film. The film also stars Emma Thompson, Robert Sean Leonard, Denzel Washington, Michael Keaton, Keanu Reeves, and Kate Beckinsale in her film debut.'},
+
+        {name: 'Joker',
+        id:'joker',
+        synopsis:'Joker is a 2019 American psychological thriller film directed and produced by Todd Phillips, who co-wrote the screenplay with Scott Silver. The film, based on DC Comics characters, stars Joaquin Phoenix as the Joker and provides a possible origin story for the character. Set in 1981, it follows Arthur Fleck, a failed stand-up comedian whose descent into insanity and nihilism inspires a violent counter-cultural revolution against the wealthy in a decaying Gotham City. Robert De Niro, Zazie Beetz, Frances Conroy, Brett Cullen, Glenn Fleshler, Bill Camp, Shea Whigham, and Marc Maron appear in supporting roles.'},
+    
+        {name: 'Inherent',
+        id: 'inherent',
+        synopsis:'Inherent Vice is a 2014 American neo-noir film written and directed by Paul Thomas Anderson, based on the 2009 novel of the same name by Thomas Pynchon. The cast includes Joaquin Phoenix, Josh Brolin, Owen Wilson, Katherine Waterston, Eric Roberts, Reese Witherspoon, Benicio del Toro, Jena Malone, Joanna Newsom, Jeannie Berlin, Maya Rudolph, Michael K. Williams and Martin Short. The film follows Larry "Doc" Sportello, a well-intentioned but inept stoner, hippie, and private investigator in 1970, who is embroiled in the Los Angeles criminal underworld while investigating three cases interrelated by the disappearance of his ex-girlfriend and her new wealthy boyfriend.'},
+    
+        {name: 'Underwater',
+        id: 'underwater',
+        synopsis:'Underwater is a 2020 American science fiction horror film[4] directed by William Eubank and written by Brian Duffield and Adam Cozad. The film is produced by Chernin Entertainment and stars Kristen Stewart, Vincent Cassel, Jessica Henwick, John Gallagher Jr., Mamoudou Athie, and T.J. Miller.[5] It follows a group of scientists at the bottom of the ocean who encounter a group of creatures after an earthquake destroys their laboratory. The film is inspired by the Cthulhu Mythos as it includes the appearance of Cthulhu, and as such is a loose adaptation of The Call of Cthulhu.'}
+    
+    ];
+};
