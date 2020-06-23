05518c56d6927968183f9f7db64579f1a01fbee1
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-06-23 15:44:33 +0100

    Reconfigure task #21

--- src/backend/app.js
+++ src/backend/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const getShows = require('./shows');
 
+app.get('/', (req, res) => res.send('Hello'));
+
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
 app.listen(3001, () => console.log(`Example app listening on port 3001!!!!`));
