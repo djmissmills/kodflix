02edf170c79f91ecb6b8064ac71c0fe6a9149d4e
Lorraine <48839557+djmissmills@users.noreply.github.com>
2020-06-24 21:29:26 +0100

    heroku

--- src/backend/app.js
+++ src/backend/app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const port = process.env.PORT || 3001;
 const getShows = require('./shows');
 
 // app.get('/', (req, res) => res.send('Hello'));
 
 app.get('/rest/shows', (req, res) => res.send(getShows()));
 
-app.listen(3001, () => console.log(`Example app listening on port 3001!!!!`));
+// Serve any static files
+app.use(express.static(path.join(__dirname, '../../build')));
+// Handle React routing, return all requests to React app
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '../../build', 'index.html'));
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`)); 
